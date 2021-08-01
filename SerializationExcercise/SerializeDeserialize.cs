4c29b55c7a36cb47e6c61f3e47d7c0793da56413
Petya Todorova <invisioncl@gmail.com>
2021-08-01 22:38:33 +0300

    Serialization or Deserialization using Newtonsoft.

---
+++ SerializationExcercise/SerializeDeserialize.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ConsoleApp2
+{
+    class SerializeDeserialize
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Oil pump",
+                Description = null,
+                Cost = 25
+            };
+
+            var jsonProduct = JsonConvert.SerializeObject(product, Formatting.Indented);
+
+            Console.WriteLine(jsonProduct);
+
+            var objProduct = JsonConvert.DeserializeObject<Product>(jsonProduct);
+            Console.WriteLine($"id = {objProduct.Id}");
+            Console.WriteLine($"Name = {objProduct.Name}");
+            Console.WriteLine($"Desc = {objProduct.Description}");
+            Console.WriteLine($"Cost = {objProduct.Cost}");
+
+            
+        }
+    }
+}
